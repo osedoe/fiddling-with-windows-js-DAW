827891ca60780fa46ed6811f12658bdc6c47ad0e
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-10-22 13:50:56 +0100

    Added basic html layout

---
+++ app.js
@@ -0,0 +1,12 @@
+// index.html variables
+const output = document.querySelector('#output');
+const btnOpen = document.querySelector('#btn-open');
+const BtnContent = document.querySelector('#btn-content');
+
+// form.html variables
+const input = document.querySelector('#input');
+const btnSend = document.querySelector('#btn-send');
+
+document.addEventListener('click', () => {
+
+});
